f48fc5d4bacd6078ef40590829c13c11907edcdc
Matheus Pires de Sousa <sousa@bne-empregos.com.br>
2022-09-30 23:52:10 -0300

    Project QRCode-Scanner init, created function scan

---
+++ App.tsx
@@ -0,0 +1,8 @@
+import 'react-native-reanimated';
+import Home from './src/screens/Home';
+
+const App = () => {
+  return <Home />;
+};
+
+export default App;
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components/native';
+import {TouchableOpacity} from 'react-native';
+
+export const Container = styled.View`
+  flex: 1;
+`;
+
+export const CameraWrapper = styled.View`
+  flex: 1;
+  width: 100%;
+`;
+export const ButtonWrapper = styled(TouchableOpacity)``;
